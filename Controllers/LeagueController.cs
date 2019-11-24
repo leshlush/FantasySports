d272a71a2602dbfc5353040dfc6feb0bbfbf1fe9
Brent <brentfsanderson@gmail.com>
2019-11-24 16:21:55 -0600

    Added simple form to name and create new leagues.

---
+++ Controllers/LeagueController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FantasySports.DataAccess;
+
+namespace FantasySports.Controllers
+{
+    public class LeagueController : Controller
+    {
+        public ViewResult CreateLeague()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult CreateLeague(String leagueName)
+        {
+            LeagueData.addLeague(leagueName);
+            return Content("Form Submitted!");
+        }
+    }
+}
